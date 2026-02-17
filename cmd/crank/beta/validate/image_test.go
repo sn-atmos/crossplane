a107764b6b0f1f85309c93a9c95667f4e4ff9319
Nikita Zakharov <nikita.zakharov@statnett.no>
2026-02-17 09:18:07 +0100

    Revert "fix(beta/validate): support semantic version range in package dependencies (#17)"
    This reverts commit 47bd1f4eb552b3a5eb55c1eb6bea18b1118b4181.

--- cmd/crank/beta/validate/image_test.go
+++ cmd/crank/beta/validate/image_test.go
@@ -65,16 +65,6 @@ func TestFindImageTagForVersionConstraint(t *testing.T) {
 			constraint:   ">4.5.6",
 			expectError:  true,
 		},
-		"RangedConstraint": {
-			responseBody:  responseTags,
-			constraint:    ">=v2.0.0 <v5.0.0",
-			expectedImage: "ubuntu:4.5.6",
-		},
-		"CommaSeparatedRangedConstraint": {
-			responseBody:  responseTags,
-			constraint:    ">=v2.0.0,<v5.0.0",
-			expectedImage: "ubuntu:4.5.6",
-		},
 	}
 
 	for name, tc := range cases {
